3697f926d7771b2f87889bdaef6042439dd4d84c
Gabriel Miranda <grmiranda97@gmail.com>
2019-04-10 14:23:55 -0300

    problema 1

---
+++ Problema 1/Proj_quartus/output_files/Chain6.cdf
@@ -0,0 +1,13 @@
+/* Quartus II 64-Bit Version 13.1.4 Build 182 03/12/2014 SJ Web Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE6E22) Path("C:/Users/grmir/Documents/FPGA/Sistemas Digitais/Problema 1/Proj_quartus/output_files/") File("proj_qsys.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ Problema 1/Proj_quartus/top.v
@@ -0,0 +1,21 @@
+module top (
+	input clk,
+	input [3:0] buttons,
+	output [3:0] leds,
+	output en,
+	output rw,
+	output rs,
+	output [7:0] db
+);
+
+	 proj_qsys u0 (
+        .clk_clk                            (clk),   //                            clk.clk
+        .buttons_external_connection_export (buttons),// buttons_external_connection.export
+        .leds_external_connection_export    (leds), 	//    leds_external_connection.export
+        .en_export                          (en), 		//                          en.export
+        .rw_export                          (rw),     //                          rw.export
+        .rs_export                          (rs),     //                          rs.export
+        .db_export                          (db)      //                          db.export
+    );
+
+endmodule
